b4e65ca5283356ea18a1395210401012e47fdb94
Shiv3006 <116787360+Shiv3006@users.noreply.github.com>
2024-03-01 14:26:21 +0530

    Add files via upload
    Added Actuator demo

---
+++ ActuatorDemoEx/src/main/java/com/bnt/ActuatorDemoEx/ActuatorDemoExApplication.java
@@ -0,0 +1,13 @@
+package com.bnt.ActuatorDemoEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorDemoExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorDemoExApplication.class, args);
+	}
+
+}
---
+++ ActuatorDemoEx/src/test/java/com/bnt/ActuatorDemoEx/ActuatorDemoExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnt.ActuatorDemoEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorDemoExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
